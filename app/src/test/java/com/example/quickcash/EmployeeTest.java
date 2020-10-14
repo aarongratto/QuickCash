8a0660b350b5c264d1427ada1978b3c5fed4df85
Patrick Strongman <pt809405@dal.ca>
2020-10-14 10:35:04 -0300

    Add JUnit tests for Employee/Employer classes

---
+++ app/src/test/java/com/example/quickcash/EmployeeTest.java
@@ -0,0 +1,42 @@
+package com.example.quickcash;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+    static String employeeUsername = "Patrick";
+    static String employeePassword = "password";
+    Employee employee;
+
+    @Before
+    public void setupUnit() {
+        employee = new Employee();
+        employee.login(employeeUsername, employeePassword);
+    }
+
+    @Test
+    public void testType() {
+        assertEquals("employee", employee.getUserType(), "Wrong user type returned. " +
+                "Expected: employee, returned: " + employee.getUsetType());
+    }
+
+    @Test
+    public void testUsername() {
+        assertEquals(employeeUsername, employee.getUserName(), "Wrong username returned. " +
+                "Expected: " + employeeUsername + ", returned: " + employee.getUsername());
+    }
+
+    @Test
+    public void testLogin() {
+        assertTrue(employee.isLoggedIn());
+    }
+
+    @Test
+    public void testWrongInfo() {
+        Employee employeeTest = new Employee();
+        employeeTest.login("Partick","wordpass");
+        assertFalse(employeeTest.isLoggedIn());
+    }
+}
---
+++ app/src/test/java/com/example/quickcash/EmployerTest.java
@@ -0,0 +1,7 @@
+package com.example.quickcash;
+
+import static org.junit.Assert.*;
+
+public class EmployerTest {
+
+}
