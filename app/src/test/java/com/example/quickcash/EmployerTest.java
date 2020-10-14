005bc3663d5c6a4d20d2628a53837f336d681bcd
Patrick Strongman <pt809405@dal.ca>
2020-10-14 10:39:42 -0300

    Add remaining tests for Employer

--- app/src/test/java/com/example/quickcash/EmployerTest.java
+++ app/src/test/java/com/example/quickcash/EmployerTest.java
@@ -1,7 +1,43 @@
 package com.example.quickcash;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EmployerTest {
+    static String employerUsername = "Aaron";
+    static String employerPassword = "pass";
+    Employer employer;
+
+    @Before
+    public void setupUnit() {
+        employer = new Employer();
+        employer.login(employerUsername, employerPassword);
+    }
+
+    @Test
+    public void testType() {
+        assertEquals("employee", employer.getUserType(), "Wrong user type returned. " +
+                "Expected: employee, returned: " + employer.getUsetType());
+    }
+
+    @Test
+    public void testUsername() {
+        assertEquals(employerUsername, employer.getUserName(), "Wrong username returned. " +
+                "Expected: " + employerUsername + ", returned: " + employer.getUsername());
+    }
+
+    @Test
+    public void testLogin() {
+        assertTrue(employer.isLoggedIn());
+    }
+
+    @Test
+    public void testWrongInfo() {
+        Employer employerTest = new Employer();
+        employerTest.login("Partick","wordpass");
+        assertFalse(employerTest.isLoggedIn());
+    }
 
 }
